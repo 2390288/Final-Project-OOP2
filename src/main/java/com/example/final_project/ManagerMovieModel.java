630eb8e21242c42559ac0e14fff10ae15fd6b035
Cohen Hopkins <cohen.hopkins@stu.crc-lennox.qc.ca>
2024-11-25 13:03:13 -0500

    Added Comments

--- src/main/java/com/example/final_project/ManagerMovieModel.java
+++ src/main/java/com/example/final_project/ManagerMovieModel.java
@@ -5,49 +5,107 @@ import java.util.List;
 
 /**
  * Represents a movie in the Movie Theatre Management System.
+ * This model contains the essential details about a movie,
+ * including its title and genre.
  */
 public class ManagerMovieModel {
-    private String title;
-    private String genre;
-  //  private List<Showtime> availableShowtimes; // Association with Showtime class
 
-    // Constructor
+    /**
+     * The title of the movie.
+     */
+    private String title;
+
+    /**
+     * The genre of the movie.
+     */
+    private String genre;
+
+    // Uncomment the following if showtimes need to be managed:
+    // private List<Showtime> availableShowtimes; // Association with Showtime class
+
+    /**
+     * Constructs a new {@code ManagerMovieModel} with the specified title and genre.
+     *
+     * @param title the title of the movie.
+     * @param genre the genre of the movie.
+     */
     public ManagerMovieModel(String title, String genre) {
         this.title = title;
         this.genre = genre;
-       // this.availableShowtimes = new ArrayList<>();
+        // Uncomment to initialize showtimes if needed:
+        // this.availableShowtimes = new ArrayList<>();
     }
 
-    // Getters and setters
+    /**
+     * Retrieves the title of the movie.
+     *
+     * @return the title of the movie.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Updates the title of the movie.
+     *
+     * @param title the new title of the movie.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Retrieves the genre of the movie.
+     *
+     * @return the genre of the movie.
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Updates the genre of the movie.
+     *
+     * @param genre the new genre of the movie.
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
- //   public List<Showtime> getAvailableShowtimes() {
-   //     return availableShowtimes;
-    //}
+    // I will uncomment the following methods when I make the showtime model:
+    /*
+    /**
+     * Retrieves the list of available showtimes for the movie.
+     *
+     * @return a list of available showtimes.
+     */
+    // public List<Showtime> getAvailableShowtimes() {
+    //     return availableShowtimes;
+    // }
 
- //   public void addShowtime(Showtime showtime) {
-   //     this.availableShowtimes.add(showtime);
-    //}
+    /**
+     * Adds a new showtime for the movie.
+     *
+     * @param showtime the showtime to be added.
+     */
+    // public void addShowtime(Showtime showtime) {
+    //     this.availableShowtimes.add(showtime);
+    // }
 
-    //public void removeShowtime(Showtime showtime) {
-     //   this.availableShowtimes.remove(showtime);
-    //}
+    /**
+     * Removes a showtime for the movie.
+     *
+     * @param showtime the showtime to be removed.
+     */
+    // public void removeShowtime(Showtime showtime) {
+    //     this.availableShowtimes.remove(showtime);
+    // }
 
-    // toString method for debugging
+    /**
+     * Returns a string representation of the movie object for debugging purposes.
+     *
+     * @return a string containing the movie's title and genre.
+     */
     @Override
     public String toString() {
         return "Movie{" +
